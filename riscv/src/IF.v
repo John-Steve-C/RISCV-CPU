b5143e93d4b8848f994632fbfdb03fd9773afe1c
John-Steve-C <leonardchen701@outlook.com>
2022-11-03 00:01:35 +0800

    pre

--- riscv/src/IF.v
+++ riscv/src/IF.v
@@ -8,8 +8,10 @@ module IF (
 );
 
 reg [31:0] pc;
-reg stall;
+reg stall, carryUp;
 wire [31:0] icache_instr;
+assign imm = 0;
+assign predict_pc = pc + imm;
 
 always @(posedge clk_in)
   begin
@@ -17,7 +19,8 @@ always @(posedge clk_in)
       begin
         pc <= 0;
         stall <= 0;
-        
+        carryUp <= 0;
+
       end
     else if (!rdy_in)
       begin
@@ -25,7 +28,8 @@ always @(posedge clk_in)
       end
     else
       begin
-        
+        carryUp <= 0;
+
       end
   end
 
